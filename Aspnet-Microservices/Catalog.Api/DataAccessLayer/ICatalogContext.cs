ee4e1e8e8fac7150e5079670198600c488e3a685
mokhtar ahmadi <iammokhtarahmadi@gmail.com>
2021-12-28 13:42:14 +0330

    catalog

---
+++ Aspnet-Microservices/Catalog.Api/DataAccessLayer/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Api.Entity;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.DataAccessLayer
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
